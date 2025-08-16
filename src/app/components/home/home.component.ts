e5b8133eee72ddaf45d1c1abef3afdb833ac5f6a
dkahonbu <dkahonbu@gmail.com>
2025-08-16 19:49:25 +0800

    aboutus - services - teams push

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { faAlignLeft } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faTwitter, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faAlignLeft, faUsersViewfinder } from '@fortawesome/free-solid-svg-icons';
 
 declare var WOW: any;
 declare var spinner: any;
@@ -12,7 +13,12 @@ declare var type: any;
 })
 export class HomeComponent implements AfterViewInit {
   faAlignLeft = faAlignLeft;
-  
+  faFacebookF = faFacebookF;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedin;
+  faInstagram = faInstagram;
+  faUsersViewfinder = faUsersViewfinder;
+
   ngAfterViewInit(): void {
     new WOW().init();
     new type();
