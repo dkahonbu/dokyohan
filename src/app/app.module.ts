e47ae9ae5038bb03702c122ef252950a292e6689
dkahonbu <dkahonbu@gmail.com>
2025-08-16 13:07:07 +0800

    first push

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { EventsComponent } from './components/events/events.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    TeamsComponent,
+    EventsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
+    
+  ],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"dka-honbu-f71c2","appId":"1:174889089136:web:9c1ce5d6c5b5a0cca8dcb5","storageBucket":"dka-honbu-f71c2.firebasestorage.app","apiKey":"AIzaSyCf2Yrm15hAWa-DAwygZlPLlExqaJlzUX0","authDomain":"dka-honbu-f71c2.firebaseapp.com","messagingSenderId":"174889089136","measurementId":"G-3087C5V4VY"})),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCf2Yrm15hAWa-DAwygZlPLlExqaJlzUX0",
+        authDomain: "dka-honbu-f71c2.firebaseapp.com",
+        projectId: "dka-honbu-f71c2",
+        storageBucket: "dka-honbu-f71c2.firebasestorage.app",
+        messagingSenderId: "174889089136",
+        appId: "1:174889089136:web:9c1ce5d6c5b5a0cca8dcb5",
+        measurementId: "G-3087C5V4VY"
+    }
+};
